cd0a3de01976d09ab74446aa47a6519c35cad0d5
mbalcer <mateusz75319@gmail.com>
2020-08-30 21:56:41 +0200

    Created user details service implementation

---
+++ enrollment-system-api/src/main/java/pl/mbalcer/enrollmentsystem/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package pl.mbalcer.enrollmentsystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.mbalcer.enrollmentsystem.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return userRepository.findUserByUsername(s).orElseThrow(() -> new UsernameNotFoundException(s));
+    }
+}
