1e3f1413d779373039cdce64fa80d435c566ea62
mbalcer <mateusz75319@gmail.com>
2020-09-01 13:52:28 +0200

    Added basic auth controller

---
+++ enrollment-system-api/src/main/java/pl/mbalcer/enrollmentsystem/model/AuthenticationBean.java
@@ -0,0 +1,10 @@
+package pl.mbalcer.enrollmentsystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthenticationBean {
+    private String message;
+}
