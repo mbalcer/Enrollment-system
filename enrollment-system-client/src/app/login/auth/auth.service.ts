fb837eff640ed1997170142a5685c06208b75d6d
mbalcer <mateusz75319@gmail.com>
2020-09-09 22:45:23 +0200

    Implemented authorization service

--- enrollment-system-client/src/app/login/auth/auth.service.ts
+++ enrollment-system-client/src/app/login/auth/auth.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser';
 
-  constructor() { }
+  public username: string;
+
+  constructor(private http: HttpClient) {}
+
+  authenticationService(username: string, password: string) {
+    return this.http.get(environment.basePath + `/auth/basic`,
+      { headers: { authorization: this.createBasicAuthToken(username, password) } }).pipe(map(() => {
+      this.username = username;
+      this.registerSuccessfulLogin(username);
+    }));
+  }
+
+  createBasicAuthToken(username: string, password: string) {
+    return 'Basic ' + window.btoa(username + ':' + password);
+  }
+
+  registerSuccessfulLogin(username) {
+    sessionStorage.setItem(this.USER_NAME_SESSION_ATTRIBUTE_NAME, username);
+  }
+
+  logout() {
+    sessionStorage.removeItem(this.USER_NAME_SESSION_ATTRIBUTE_NAME);
+    this.username = null;
+  }
+
+  isUserLoggedIn() {
+    const user = sessionStorage.getItem(this.USER_NAME_SESSION_ATTRIBUTE_NAME);
+    if (user === null) { return false; }
+    return true;
+  }
+
+  getLoggedInUserName() {
+    const user = sessionStorage.getItem(this.USER_NAME_SESSION_ATTRIBUTE_NAME);
+    if (user === null) { return ''; }
+    return user;
+  }
+
 }
--- enrollment-system-client/src/app/login/login.component.ts
+++ enrollment-system-client/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthService} from './auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userToLogin: LoginUserViewModel = {
+    username: '',
+    password: ''
+  };
+  errorMessage = 'Invalid Credentials';
+  successMessage: string;
+  invalidLogin = false;
+  loginSuccess = false;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private router: Router, private authService: AuthService) { }
+
+  ngOnInit(): void { }
+
+  handleLogin() {
+    this.authService.authenticationService(this.userToLogin.username, this.userToLogin.password).subscribe(result => {
+      this.invalidLogin = false;
+      this.loginSuccess = true;
+      this.successMessage = 'Login Successful';
+      this.router.navigate(['/admin-panel']);
+    }, () => {
+      this.invalidLogin = true;
+      this.loginSuccess = false;
+    });
   }
+}
 
+export interface LoginUserViewModel {
+  username: string;
+  password: string;
 }
