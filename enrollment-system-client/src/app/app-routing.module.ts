67dc2fd1433825518b153c49ed96a3d78f80ba94
mbalcer <mateusz75319@gmail.com>
2020-09-16 21:27:09 +0200

    Added auth guard which check is user logged in

--- enrollment-system-client/src/app/app-routing.module.ts
+++ enrollment-system-client/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import {AdminPanelComponent} from './admin-panel/admin-panel.component';
+import {AuthGuard} from './login/auth/auth.guard';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: '', component: LoginComponent},
-  {path: 'admin-panel', component: AdminPanelComponent},
+  {path: 'admin-panel', component: AdminPanelComponent, canActivate: [AuthGuard]},
   {path: 'logout', component: LoginComponent},
 ];
 
