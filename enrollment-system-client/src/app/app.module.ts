1909414127e0775c1b0218309260053c2fba96b3
mbalcer <mateusz75319@gmail.com>
2020-09-17 22:29:01 +0200

    Created register component

--- enrollment-system-client/src/app/app.module.ts
+++ enrollment-system-client/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {AdminPanelComponent} from './admin-panel/admin-panel.component';
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {HttpInterceptorService} from './http-interceptor.service';
+import {RegisterComponent} from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AdminPanelComponent
+    AdminPanelComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
