5a0f6161673855b7e33d25ffc610cc04180a8eb3
mbalcer <mateusz75319@gmail.com>
2020-09-19 16:05:45 +0200

    Added register page template

--- enrollment-system-client/src/app/app-routing.module.ts
+++ enrollment-system-client/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
 import {AdminPanelComponent} from './admin-panel/admin-panel.component';
 import {AuthGuard} from './login/auth/auth.guard';
+import {RegisterComponent} from './register/register.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: '', component: LoginComponent},
   {path: 'admin-panel', component: AdminPanelComponent, canActivate: [AuthGuard]},
   {path: 'logout', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
--- enrollment-system-client/src/app/register/register.component.ts
+++ enrollment-system-client/src/app/register/register.component.ts
@@ -3,13 +3,29 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['../login/login.component.scss', './register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+
+  userToRegister: UserRegisterViewModel = {
+    login: '',
+    password: '',
+    confirmPassword: '',
+    email: ''
+  };
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  signUp() {
+  }
+}
+
+export interface UserRegisterViewModel {
+  login: string;
+  password: string;
+  confirmPassword: string;
+  email: string;
 }
