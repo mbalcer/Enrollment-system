ed1ab455634027d80d3589349ed9b6f7f60addc4
mbalcer <mateusz75319@gmail.com>
2020-09-26 15:37:15 +0200

    Created dashboard component

--- enrollment-system-client/src/app/app-routing.module.ts
+++ enrollment-system-client/src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
-import {AdminPanelComponent} from './admin-panel/admin-panel.component';
 import {AuthGuard} from './login/auth/auth.guard';
 import {RegisterComponent} from './register/register.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: '', component: LoginComponent},
-  {path: 'admin-panel', component: AdminPanelComponent, canActivate: [AuthGuard]},
   {path: 'logout', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- enrollment-system-client/src/app/app.module.ts
+++ enrollment-system-client/src/app/app.module.ts
@@ -3,19 +3,19 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
-import {AdminPanelComponent} from './admin-panel/admin-panel.component';
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {HttpInterceptorService} from './http-interceptor.service';
 import {RegisterComponent} from './register/register.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AdminPanelComponent,
-    RegisterComponent
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
