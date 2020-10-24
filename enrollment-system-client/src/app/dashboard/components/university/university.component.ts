0ce2886df85ae85402ca02c72c3dea76bc764c7a
mbalcer <mateusz75319@gmail.com>
2020-10-24 19:19:11 +0200

    Added university template where displaying faculties

--- enrollment-system-client/src/app/dashboard/components/university/university.component.ts
+++ enrollment-system-client/src/app/dashboard/components/university/university.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {Faculty, IFaculty} from './faculty.model';
+import {FacultyService} from './faculty.service';
 
 @Component({
   selector: 'app-university',
@@ -6,10 +9,38 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./university.component.scss']
 })
 export class UniversityComponent implements OnInit {
+  displayedColumnsFacultyTable: string[] = ['id', 'name', 'address', 'abbreviation', 'actions'];
+  dataSource = new MatTableDataSource<Faculty>();
+  faculties: IFaculty[];
 
-  constructor() { }
+  constructor(private facultyService: FacultyService) { }
 
   ngOnInit(): void {
+    this.getFaculties();
   }
 
+  refreshTable() {
+    this.dataSource = new MatTableDataSource<Faculty>(this.faculties);
+  }
+
+  getFaculties() {
+    this.facultyService.getAllFaculties().subscribe(result => {
+      this.faculties = result;
+      this.refreshTable();
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  deleteFaculty(row: IFaculty) {
+    console.log(row);
+  }
+
+  editFaculty(row: IFaculty) {
+    console.log(row);
+  }
+
+  addFaculty() {
+    console.log("add");
+  }
 }
