be1b9977e7c694daeb7640574d6277f64d5ddd77
mbalcer <mateusz75319@gmail.com>
2020-11-01 22:37:16 +0100

    Added displaying subject group table

--- enrollment-system-client/src/app/dashboard/components/groups/groups.component.ts
+++ enrollment-system-client/src/app/dashboard/components/groups/groups.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {ISubjectGroup, SubjectGroup} from './subject-group.model';
+import {SubjectGroupService} from './subject-group.service';
 
 @Component({
   selector: 'app-groups',
@@ -6,10 +9,35 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./groups.component.scss']
 })
 export class GroupsComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'subject', 'teacher', 'fieldsOfStudy', 'numberOfEnrolled', 'numberOfPlaces'];
+  dataSource = new MatTableDataSource<SubjectGroup>();
+  groups: ISubjectGroup[];
 
-  constructor() { }
+  constructor(private subjectGroupService: SubjectGroupService) { }
 
   ngOnInit(): void {
+    this.getGroups();
   }
 
+  refreshTable() {
+    this.dataSource = new MatTableDataSource<SubjectGroup>(this.groups);
+  }
+
+  getGroups() {
+    this.subjectGroupService.getAllGroups().subscribe(result => {
+      console.log(result);
+      this.groups = result;
+      console.log(this.groups);
+      this.refreshTable();
+    }, error => console.log(error));
+  }
+
+  getListFieldsOfStudy(group: SubjectGroup) {
+    let list = '';
+    group.fieldsOfStudyDTO.forEach(fieldOfStudy => {
+      list += fieldOfStudy.name + ', ';
+    });
+    list = list.substr(0, list.length - 2);
+    return list;
+  }
 }
