f1e57aeb528df751669a7d362626c08edc709e23
mbalcer <mateusz75319@gmail.com>
2020-11-28 20:03:56 +0100

    Added form to add/edit news

--- enrollment-system-client/src/app/dashboard/components/home/add-news/add-news.component.ts
+++ enrollment-system-client/src/app/dashboard/components/home/add-news/add-news.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {NewsService} from '../news.service';
+import {News} from '../news.model';
 
 @Component({
   selector: 'app-add-news',
@@ -6,10 +9,34 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./add-news.component.scss']
 })
 export class AddNewsComponent implements OnInit {
+  isAdd = true;
+  newsToEdit = new News();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private newsService: NewsService, private router: Router) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id != null) {
+      this.isAdd = false;
+      this.getNews(Number(id));
+    }
   }
 
+  getNews(id: number) {
+    this.newsService.getNews(id).subscribe(result => {
+      this.newsToEdit = result;
+    }, error => console.log(error));
+  }
+
+  saveNews() {
+    if (this.isAdd) {
+      this.newsService.postNews(this.newsToEdit).subscribe(result => {
+        this.newsToEdit = new News();
+      }, error => console.log(error));
+    } else {
+      this.newsService.putNews(this.newsToEdit, this.newsToEdit.id).subscribe(result => {
+        this.router.navigateByUrl('/dashboard/news/add');
+      }, error => console.log(error));
+    }
+  }
 }
