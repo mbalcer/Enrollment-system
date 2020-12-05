9c4ca8dad996e90b07992e98fe070787dbaaef57
mbalcer <mateusz75319@gmail.com>
2020-12-05 22:54:26 +0100

    Added edit profile page template

--- enrollment-system-client/src/app/dashboard/components/profile/edit-profile/edit-profile.component.ts
+++ enrollment-system-client/src/app/dashboard/components/profile/edit-profile/edit-profile.component.ts
@@ -1,4 +1,15 @@
 import {Component, OnInit} from '@angular/core';
+import {IUser, User} from '../../../../user/model/user.model';
+import {TokenStorageService} from '../../../../user/auth/token-storage.service';
+import {UserService} from '../../../../user/service/user.service';
+import {StudentService} from '../../../../user/service/student.service';
+import {TeacherService} from '../../../../user/service/teacher.service';
+import {IStudent} from '../../../../user/model/student.model';
+import {ITeacher} from '../../../../user/model/teacher.model';
+import {FieldOfStudyService} from '../../university/field-of-study.service';
+import {IFieldOfStudy} from '../../university/field-of-study.model';
+import {FacultyService} from '../../university/faculty.service';
+import {IFaculty} from '../../university/faculty.model';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,11 +17,72 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./edit-profile.component.scss']
 })
 export class EditProfileComponent implements OnInit {
+  user: IUser = new User();
+  student: IStudent = null;
+  teacher: ITeacher = null;
 
-  constructor() {
+  fieldsOfStudy: IFieldOfStudy[] = [];
+  faculties: IFaculty[] = [];
+
+  constructor(private tokenStorageService: TokenStorageService,
+              private userService: UserService,
+              private studentService: StudentService,
+              private teacherService: TeacherService,
+              private fieldOfStudyService: FieldOfStudyService,
+              private facultyService: FacultyService) {
   }
 
   ngOnInit(): void {
+    this.getUser();
+    this.getFieldsOfStudy();
+    this.getFaculties();
   }
 
+  public objectComparisonFunction(option, value): boolean {
+    return option.id === value.id;
+  }
+
+  getFieldsOfStudy() {
+    this.fieldOfStudyService.getAllFieldsOfStudy().subscribe(result => {
+      this.fieldsOfStudy = result;
+    }, error => console.log(error));
+  }
+
+  getFaculties() {
+    this.facultyService.getAllFaculties().subscribe(result => {
+      this.faculties = result;
+    }, error => console.log(error));
+  }
+
+  getUser() {
+    const user = this.tokenStorageService.getUser();
+    const role = this.tokenStorageService.getActiveRole();
+    if (role === 'STUDENT') {
+      this.studentService.getStudentByUsername(user.username).subscribe(result => {
+        this.user = result;
+        this.student = result;
+      }, err => console.log(err));
+    } else if (role === 'TEACHER') {
+      this.teacherService.getTeacherByUsername(user.username).subscribe(result => {
+        this.user = result;
+        this.teacher = result;
+      }, err => console.log(err));
+    } else {
+      this.userService.getUser().subscribe(result => {
+        this.user = result;
+      }, err => console.log(err));
+    }
+  }
+
+  editUser() {
+
+  }
+
+  editStudent() {
+
+  }
+
+  editTeacher() {
+
+  }
 }
