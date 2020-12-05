ddf43c4620287aa03e2153241b7a3c820e2bf1ce
mbalcer <mateusz75319@gmail.com>
2020-12-05 18:30:39 +0100

    Changed filter users table and remove changing the role in this table

--- enrollment-system-client/src/app/dashboard/components/users/users.component.ts
+++ enrollment-system-client/src/app/dashboard/components/users/users.component.ts
@@ -13,10 +13,6 @@ export class UsersComponent implements OnInit {
   displayedColumns: string[] = ['username', 'email', 'fullName', 'role', 'active'];
   dataSource = new MatTableDataSource<User>();
   users: IUser[];
-  roles = ['STUDENT', 'TEACHER', 'ADMIN'];
-  filterRoles = ['ALL'].concat(this.roles);
-  editableRow = null;
-  disableEditRow = false;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
@@ -29,45 +25,18 @@ export class UsersComponent implements OnInit {
   getUsers() {
     this.userService.getAllUsers().subscribe(result => {
       this.users = result;
+      console.log(result);
       this.refreshTables();
     }, error => console.log(error));
   }
-
 
   refreshTables() {
     this.dataSource = new MatTableDataSource<User>(this.users);
     this.dataSource.paginator = this.paginator;
   }
 
-  filterTable(filterValue) {
-    if (filterValue.isUserInput) {
-      filterValue = filterValue.source.value;
-      if (filterValue === 'ALL') {
-        this.dataSource.filter = '';
-      } else {
-        this.dataSource.filter = filterValue;
-      }
-    }
-  }
-
-  changeRole(element: IUser) {
-    this.userService.changeRole(element).subscribe(result => {
-      this.refreshTables();
-    }, error => console.log(error));
-    this.cancelChangeRole();
-  }
-
-  displayEditableRow(element: IUser) {
-    if (this.editableRow === null && !this.disableEditRow) {
-      this.editableRow = element.username;
-    }
-    if (this.disableEditRow) {
-      this.disableEditRow = false;
-    }
-  }
-
-  cancelChangeRole() {
-    this.editableRow = null;
-    this.disableEditRow = true;
+  filterTable(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
