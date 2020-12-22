affa0d622496449c0e71ea816c4bf533c4e46407
mbalcer <mateusz75319@gmail.com>
2020-12-23 00:59:52 +0100

    Added setting start registration date in settings component

--- enrollment-system-client/src/app/dashboard/components/settings/settings.component.ts
+++ enrollment-system-client/src/app/dashboard/components/settings/settings.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {FacultyService} from '../university/faculty.service';
+import {NotificationsService} from 'angular2-notifications';
+import {IFaculty} from '../university/faculty.model';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +10,25 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  faculties: IFaculty[] = [];
 
-  constructor() { }
+  constructor(private facultyService: FacultyService, private notificationService: NotificationsService) { }
 
   ngOnInit(): void {
+    this.getFaculties();
   }
 
+  getFaculties() {
+    this.facultyService.getAllFaculties().subscribe(result => {
+      this.faculties = result;
+    }, err => this.notificationService.error(err.status + ': ' + err.error.status, err.error.message));
+  }
+
+  saveRegistrationDate(faculty: IFaculty) {
+    faculty.startRegistration = new DatePipe('en-US').transform(faculty.startRegistration, 'yyyy-MM-ddTHH:mm:ss');
+    this.facultyService.putFaculty(faculty).subscribe(result => {
+      this.faculties[this.faculties.indexOf(faculty)] = result;
+      this.notificationService.success("You set start registration date for " + result.name + " on " + result.startRegistration);
+    }, err => this.notificationService.error(err.status + ': ' + err.error.status, err.error.message))
+  }
 }
