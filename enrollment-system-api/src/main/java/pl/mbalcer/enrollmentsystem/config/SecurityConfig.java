65ff2d1889725b903fbadda0df43b57463bc5a38
mbalcer <mateusz75319@gmail.com>
2021-01-02 21:04:22 +0100

    Added security config

--- enrollment-system-api/src/main/java/pl/mbalcer/enrollmentsystem/config/SecurityConfig.java
+++ enrollment-system-api/src/main/java/pl/mbalcer/enrollmentsystem/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.mbalcer.enrollmentsystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -53,7 +54,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .authorizeRequests()
+                .antMatchers("/api/auth/changePassword").authenticated()
+                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/faculty/blocked/**").hasAuthority("STUDENT")
+                .antMatchers(HttpMethod.GET, "/api/faculty/**").authenticated()
+                .antMatchers("/api/faculty/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/fieldOfStudy/**").authenticated()
+                .antMatchers("/api/fieldOfStudy/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/news/**").authenticated()
+                .antMatchers("/api/news/**").hasAuthority("ADMIN")
+                .antMatchers("/api/pdf/**").hasAuthority("TEACHER")
+                .antMatchers("/api/roles/**").authenticated()
+                .antMatchers(HttpMethod.GET, "/api/student/**").authenticated()
+                .antMatchers("/api/student/**").hasAuthority("STUDENT")
+                .antMatchers(HttpMethod.GET, "/api/subject/**").authenticated()
+                .antMatchers("/api/subject/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/subjectGroup/requests").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/subjectGroup/byTeacher/**").hasAuthority("TEACHER")
+                .antMatchers(HttpMethod.GET, "/api/subjectGroup/registration/**").hasAuthority("STUDENT")
+                .antMatchers(HttpMethod.GET, "/api/subjectGroup/**").authenticated()
+                .antMatchers(HttpMethod.POST, "/api/subjectGroup/**").hasAnyAuthority("ADMIN", "TEACHER")
+                .antMatchers(HttpMethod.DELETE, "/api/subjectGroup/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/subjectGroup/add/**").hasAuthority("STUDENT")
+                .antMatchers(HttpMethod.PUT, "/api/subjectGroup/remove/**").hasAuthority("STUDENT")
+                .antMatchers(HttpMethod.PUT, "/api/subjectGroup/type/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/subjectGroup/**").hasAnyAuthority("ADMIN", "TEACHER")
+                .antMatchers(HttpMethod.GET, "/api/teacher/**").authenticated()
+                .antMatchers("/api/teacher/**").hasAuthority("TEACHER")
+                .antMatchers(HttpMethod.GET, "/api/users/**").authenticated()
+                .antMatchers(HttpMethod.PUT, "/api/users/role/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/users/**").authenticated()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
